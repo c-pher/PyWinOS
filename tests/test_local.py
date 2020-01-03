4f22ef980bbc68b14d32ebcb3977ead2b04222bd
agegemon <a.komisssarov@gmail.com>
2020-01-04 00:36:07 +0200

    Tests
    test_is_host_available.py added
    test_local.py extended

--- tests/test_local.py
+++ tests/test_local.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 from pywinos import WinOSClient
@@ -5,6 +7,23 @@ from pywinos import WinOSClient
 
 @pytest.mark.parametrize('command', ['whoami', 'hostname', 'arp -a'])
 def test_run_cmd_local(command):
-    tool = WinOSClient()
+    tool = WinOSClient(host='')
     response = tool.run_cmd(command=command)
     assert response.ok, 'Response is not OK'
+
+
+def test_run_cmd_invalid_command():
+    tool = WinOSClient(host='')
+    response = tool.run_cmd('whoamia')
+    assert not response.ok, 'Response is OK. Must be False'
+    assert not response.stdout, 'STDOUT is not empty. Must be empty'
+    assert 'is not recognized as an internal' in response.stderr, \
+        'Response is OK. Must be False'
+
+
+def test_get_local_hostname_ip():
+    tool = WinOSClient(host='')
+    response = tool.get_local_hostname_ip()
+    ip_regex = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+
+    assert ip_regex.match(response.ip), 'IP address not found'
